fc11b634fb2e6155be57da12ccfda4afca795385
Osava25 <contacto@ideamosweb.com>
2023-09-02 19:38:50 -0500

    Loader y corrección bug conexion google sheet

--- view.js
+++ view.js
@@ -12,7 +12,9 @@ const guestNumberDom = document.getElementById('guest_number')
 const emptyName = document.getElementById('empty_name');
 const validationName = document.getElementById('validation-name');
 let formGuess = document.getElementById("form_guess");
-let textGuess = document.getElementById("text_guess") 
+let textGuess = document.getElementById("text_guess");
+let loadValidation = document.getElementById("loader");
+let btnComplete = document.getElementById("btn-complete"); 
 
 // CLASE INVITADOS
 class Guest{
@@ -131,7 +133,7 @@ function addInfoGuess(){
 
           if(guestFound.subGuest != null){
             guestFound.subGuest.forEach(element => {
-              guestNumber.innerHTML += `<input type="radio" name="toggle" class="accordion-toggle" checked /><div class="accordion-header">Invitado Titular ${element.name} ${element.lastname}</div><div class="accordion-content caja-info-acordeon">
+              guestNumber.innerHTML += `<input type="radio" name="toggle" class="accordion-toggle" /><div class="accordion-header">${element.name} ${element.lastname}</div><div class="accordion-content caja-info-acordeon">
               <div class="spacing-top-10"><p class="descripcion"><b>¿Confirmas la asistencia de ${element.name} ${element.lastname} a la boda?</b></p>
               <div class="center-input"><input type="radio" id="si-1" name="guess_${nameGuess}" value="si"><label class="l-guest" for="no">Si</label></div>
               <div class="center-input"><input type="radio" id="no-1" name="guess_${nameGuess}" value="no"><label class="l-guest" for="no"> No</label></div><br></div>
@@ -143,20 +145,21 @@ function addInfoGuess(){
           }
 }
 
+// URL y constante Conexion Google Sheets
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzZi2ux2bGxIkRzYThzMclvMANnxsgc1XsFayq_HqHP1slG50ZgLhZuzzQKHdMoFTh0/exec'
+const form = document.forms['contact-form']
 // Validación form confirmación invitados
-if (formGuess != null) {
-  formGuess.addEventListener("submit", function(event){
+if (form != null) {
+  form.addEventListener("submit", function(event){
     var vGuess = document.getElementById("validation-guess");
     nameGuess = 2;
     if (!document.querySelector(`input[name="guess_1"]:checked`)) {
       vGuess.classList.remove("d-none");
       hasError = true;
-      console.log("checked 1")
     }
     else {
       hasError = false;
       vGuess.classList.add("d-none");
-      console.log("checked 2")
     }
    
     if (guestFound.subGuest != null){
@@ -164,32 +167,31 @@ if (formGuess != null) {
         var vSubGuess = document.getElementById("validation-guess_" + nameGuess);
       if(!document.querySelector(`input[name="guess_${nameGuess}"]:checked`)) {
         vSubGuess.classList.remove("d-none");
-        hasError = true;
-        console.log("checked 3")      
+        hasError = true;     
       } else {
         hasError = false;
         vSubGuess.classList.add("d-none");
-        console.log("checked 4")
       }
       nameGuess += 1
     });
     }
-    if(hasError) event.preventDefault();   
+    if(hasError) event.preventDefault();
+    else {
+      loadValidation.classList.remove("d-none");
+      btnComplete.classList.add("d-none");
+      // Conexión google sheet
+      event.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(() => { window.location = "gracias.html"; })
+      .catch(error => console.error('Error!', error.message))
+    }   
   })
 
 }
 
-// Conexion Google Sheets
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzZi2ux2bGxIkRzYThzMclvMANnxsgc1XsFayq_HqHP1slG50ZgLhZuzzQKHdMoFTh0/exec'
 
-const form = document.forms['contact-form']
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(() => { window.location = "gracias.html"; })
-  .catch(error => console.error('Error!', error.message))
-})
+
 
 
 
